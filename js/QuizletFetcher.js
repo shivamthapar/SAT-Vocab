819ce7ebf3e02e5a487f59e96ef125ccef512a9b
Shivam Thapar <shivam.thapar1@gmail.com>
2013-02-18 22:13:41 -0800

    added save capability plus some other stuff

--- js/QuizletFetcher.js
+++ js/QuizletFetcher.js
@@ -2,83 +2,112 @@
  * [QuizletFetcher class with numTerms amount of Vocab words fetched from Quizlet]
  * @param {[int]} numTerms [number of words needed]
  */
-function QuizletFetcher(numTerms){
+function QuizletFetcher(/*numTerms||title,array*/){ //if numTerms->fetch numTerms from Quizlet, if title,array-> localStorage set
 	var self=this; //reference to QuizletFetcher (object)
 	this.words=[]; //array of vocab words (object; word.term, word.definition)
-	this.wordsNeeded=numTerms; //amount of words still needed to be fetched (int)
 	this.cardMaker= new CardMaker();  //CardMaker object (object)
 	this.first=true; //is the first vocab word fetched? (boolean)
-	/**
-	 * [getAllWords Gets numTerms number of SAT Vocab words from Quizlet by combining sets that come up under the search term
-	 * "SAT 100 Words"]
-	 * @return {[string[]] array of fetched words}
-	 */
-	this.getAllWords = function(){
-		$.getJSON(self.makeUrl('https://api.quizlet.com/2.0/search/sets?q=SAT%20100%20Words'), function(data) {
-			var setIds=self.getDifferentSets(data, self.wordsNeeded);
-			for(var i=0; i<setIds.length; i++){
-				self.getSetTerms(setIds[i]);
-			}	
-			return self.words;	
-		});
-	}
-	/**
-	 * [getDifferentSets Returns ids of enough Quizlet sets to get num amount of terms]
-	 * @param  {[object]} data [JSON from Quizlet API call]
-	 * @param  {[int]} num  [number of terms needed]
-	 * @return {[int[]] array of ids of Quizlet Sets }
-	 */
-	this.getDifferentSets= function(data, num){
-		var ids=[];
-		var wordsFound=0;
-		var currSet=0;
-		while(wordsFound<num){
-				ids.push(data.sets[currSet].id);
-				wordsFound+=data.sets[currSet].term_count;
-				currSet++;
-		}
-		return ids;
-	}
-	/**
-	 * [getSetTerms Get words from Quizlet set with given id and append to QuizletFetcher.words array]
-	 * @param  {[int]} id [id of Quizlet set]
-	 */
-	this.getSetTerms= function(id){
-		var url= "https://api.quizlet.com/2.0/sets/"+id;
-		$.getJSON(self.makeUrl(url), function(data) {
-			var numTerms= data.terms.length;
-			var terms=[];
-			if(self.wordsNeeded<numTerms) numTerms=self.wordsNeeded;
-
-			for(var i=0;i<numTerms;i++){
-				var word= new Object();
-				word.term=data.terms[i].term;
-				word.definition=data.terms[i].definition;
-				terms.push(word);
-			}
-			for(var i=0;i<terms.length;i++){
-				var word= {"term":terms[i].term, "definition":terms[i].definition};
-				self.words.push(word);
-				self.cardMaker.createCard(terms[i].term, terms[i].definition,self.first);
+	if(arguments.length===2){
+		var title= arguments[0];
+		self.words=arguments[1];
+		this.makeCardsFromWords=function(){ //make cards from words in batch
+			for(var i=0;i<self.words.length;i++){
+				self.cardMaker.createCard(self.words[i].term, self.words[i].definition,self.first);
 				self.first=false;
-			}
-			self.wordsNeeded-=numTerms;
-			if(self.wordsNeeded==0){
 				showScreen("set");
+				activateArrowFunctions();
 			}
-		});
-	}	
-	/**
-	 * [makeUrl Helper function to append client id to Quizlet API calls]
-	 * @param  {[string (url)]} base [Quizlet API call URL]
-	 * @return {[string] URL with appended client id}
-	 */
-	this.makeUrl= function(base){
-		if(base.indexOf('?')>0)
-			return base+"&client_id=UnFvAtV9uG&whitespace=1&callback=?";
-		else
-			return base+"?client_id=UnFvAtV9uG&whitespace=1&callback=?";
+		}
+		this.makeCardsFromWords();
 	}
-	this.getAllWords(); //calls getAllWords when QuizletFetcher is first called
+	else{
+		var numTerms=arguments[0];		
+		this.wordsNeeded=numTerms; //amount of words still needed to be fetched (int)
 
+		/**
+		 * [getAllWords Gets numTerms number of SAT Vocab words from Quizlet by combining sets that come up under the search term
+		 * "SAT 100 Words"]
+		 * @return {[string[]] array of fetched words}
+		 */
+		this.getAllWords = function(){
+			$.getJSON(self.makeUrl('https://api.quizlet.com/2.0/search/sets?q=SAT%20100%20Words'), function(data) {
+				var setIds=self.getDifferentSets(data, self.wordsNeeded);
+				for(var i=0; i<setIds.length; i++){
+					self.getSetTerms(setIds[i]);
+				}	
+				return self.words;	
+			});
+		}
+		/**
+		 * [getDifferentSets Returns ids of enough Quizlet sets to get num amount of terms]
+		 * @param  {[object]} data [JSON from Quizlet API call]
+		 * @param  {[int]} num  [number of terms needed]
+		 * @return {[int[]] array of ids of Quizlet Sets }
+		 */
+		this.getDifferentSets= function(data, num){
+			var ids=[];
+			var wordsFound=0;
+			var currSet=0;
+			while(wordsFound<num){
+					ids.push(data.sets[currSet].id);
+					wordsFound+=data.sets[currSet].term_count;
+					currSet++;
+			}
+			return ids;
+		}
+		/**
+		 * [getSetTerms Get words from Quizlet set with given id and append to QuizletFetcher.words array]
+		 * @param  {[int]} id [id of Quizlet set]
+		 */
+		this.getSetTerms= function(id){
+			var url= "https://api.quizlet.com/2.0/sets/"+id;
+			$.getJSON(self.makeUrl(url), function(data) {
+				var numTerms= data.terms.length;
+				var terms=[];
+				if(self.wordsNeeded<numTerms) numTerms=self.wordsNeeded;
+
+				for(var i=0;i<numTerms;i++){
+					var word= new Object();
+					word.term=data.terms[i].term;
+					word.definition=data.terms[i].definition;
+					terms.push(word);
+				}
+				for(var i=0;i<terms.length;i++){
+					var word= {"term":terms[i].term, "definition":terms[i].definition};
+					self.words.push(word);
+					self.cardMaker.createCard(terms[i].term, terms[i].definition,self.first);
+					self.first=false;
+				}
+				self.wordsNeeded-=numTerms;
+				if(self.wordsNeeded==0){
+					showScreen("set");
+					activateArrowFunctions();
+				}
+			});
+		}	
+		/**
+		 * [makeUrl Helper function to append client id to Quizlet API calls]
+		 * @param  {[string (url)]} base [Quizlet API call URL]
+		 * @return {[string] URL with appended client id}
+		 */
+		this.makeUrl= function(base){
+			if(base.indexOf('?')>0)
+				return base+"&client_id=UnFvAtV9uG&whitespace=1&callback=?";
+			else
+				return base+"?client_id=UnFvAtV9uG&whitespace=1&callback=?";
+		}
+
+		this.saveWords= function(){
+			localStorage['set']=JSON.stringify(words);
+		}
+		/*
+		* http://dzone.com/snippets/array-shuffle-javascript
+		 */
+		function shuffle(o){
+		    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+			return o;    
+		}
+		
+		this.getAllWords(); //calls getAllWords when QuizletFetcher is first called
+	}
 }
--- js/other.js
+++ js/other.js
@@ -1,17 +1,54 @@
+var fetcher,localSet;
 $(document).ready(function(){ //when page is ready
 	showScreen("home"); //show the home screen of the app first
 	$("#new_button").click(function(){
 		showScreen("newSet");
 		$("#numTerms").focus();
 	});
-	$("#fetch_button").click(function(){
-		var num=Number($("#numTerms").val()); //get an int value from text input
-		showScreen("loading");
-		var fetcher= new QuizletFetcher(num); // create new QuizletFetcher with num amount of words		
+	$('#numTerms').on('keyup', function(e) {
+	    if (e.which == 13) {
+	        $("#fetch_button").click();
+	    }
 	});
+	$("#fetch_button").click(function(){
+		var input=$("#numTerms").val();
+		var num;
+		if((num=validateNumberInput(input)))
+		{
+			showScreen("loading");
+			fetcher= new QuizletFetcher(num); // create new QuizletFetcher with num amount of words
+		}
+		
+	});
+
+	$("#load_button").click(function(){
+		showScreen("loadScreen");
+		localSet=getLocalSet('set');
+		$("#loadedSetTitle").html(localSet.title);
+		if(localSet.words.length>0)
+			$("#loadedSetCount span").html(localSet.words.length);
+	})
+
+	$("#loadSetButton").click(function(){
+		if(localSet){
+			showScreen("loading");
+			fetcher=new QuizletFetcher(localSet.title,localSet.words);
+		}
+	})
+
 	$(".prev").live('click',prevButton);
 	$(".next").live('click',nextButton);
+	$("#saveSet").click(function(){
+		if(fetcher){
+			showScreen('saveScreen')
+		}
+	});
+	$("#saveButton").click(saveSet);
 });
+
+
+
+
 /**
  * [prevButton Show the previous card]
  */
@@ -42,4 +79,61 @@ function showScreen(name){
 	$name=$("#"+name);
 	$name.show();
 	$("#title h3").html($name.attr('data-title'));
+}
+/*
+*[validateNumberInput] validate number input
+*
+ */
+function validateNumberInput(input){
+	if(isNaN(+input))
+	{
+		generateError("Please enter a number!");
+		return false;
+	}
+	var num=Number(input); //get an int value from text input
+	if(num<0||num>200)
+	{
+		generateError("Please enter a number between 0-200!");
+		return false;
+	}
+	return num;		
+
+}
+
+function generateError(msg){
+	$("#number_input").html(msg);
+}
+
+function activateArrowFunctions(){
+	var LEFT= 37;
+	var RIGHT= 39;
+	$(document).on('keydown', function(e) {
+	    if (e.which == LEFT){
+	    	prevButton();
+	    	$(".prev").addClass('prev-hover');
+	    } 
+	    if (e.which == RIGHT){
+	    	nextButton();
+	    	$(".next").addClass('next-hover');
+	    }
+	});
+	$(document).on('keyup', function(e) {
+		$(".prev").removeClass('prev-hover');
+		$(".next").removeClass('next-hover');
+	});
+}
+
+function saveSet(){
+	if(fetcher){
+		var title= $("#nameSet").val();
+		var set= {'title':title, 'words':fetcher.words};
+		localStorage.getItem('set');
+		localStorage.setItem('set',JSON.stringify(set));
+	}
+}
+
+function getLocalSet(key){
+	if(!localStorage.getItem(key))
+		return {'title':'EMPTY','words':{}};
+	return JSON.parse(localStorage.getItem(key));
 }
